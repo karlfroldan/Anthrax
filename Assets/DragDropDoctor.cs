f362e3a7e3a4f489cef36b76825c4477ae449a49
Lance Kent Briones <lbriones@gbox.adnu.edu.ph>
2020-09-20 23:04:47 +0800

    394349415755E598FA72018D50293DBBC87BEC4A (#6)

---
+++ Assets/DragDropDoctor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragDropDoctor : MonoBehaviour{
+    public bool isDraggable = true;
+    public bool isDragged = false;
+    void Update(){
+        if (isDragged)
+            transform.position = (Vector2) Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+    private void OnMouseOver(){
+        if (isDraggable && Input.GetMouseButtonDown(0)) {
+            isDragged = true;
+        }
+    }
+    private void OnMouseUp(){
+        isDragged = false;
+    }
+}
