07f87c148260f5391fb409a7b292663e3c98bf55
satorhho <lbriones@gbox.adnu.edu.ph>
2020-09-30 11:39:50 +0800

    A1008CD3FF397D1A7BFEA59B32A39B3732BE7EDA

--- Assets/DragDropDoctor.cs
+++ Assets/DragDropDoctor.cs
@@ -5,9 +5,50 @@ using UnityEngine;
 public class DragDropDoctor : MonoBehaviour{
     public bool isDraggable = true;
     public bool isDragged = false;
+
+    private double pos_x = 11.82;       // will be updated constantly
+    private double pos_y = 3.28;        // will be updated constantly
+
+    private double original_pos_x = 11.82;      // use this to snapback to original x position
+    private double original_pos_y = 3.28;       // use this to snapback to original y position
+
+    private bool check(float x, float y) {
+        if ( (this.pos_x <= x+1 && this.pos_x >= x-1) && (this.pos_y <= y+1 && this.pos_y >= y-1) ){
+            return true;
+        }
+
+        return false;
+    }
     void Update(){
-        if (isDragged)
-            transform.position = (Vector2) Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        if (isDragged){
+            if (this.check(GameObject.Find("platform0").transform.position.x, GameObject.Find("platform0").transform.position.y)){
+                transform.position = GameObject.Find("platform0").transform.position;
+            }
+            else if (this.check(GameObject.Find("platform1").transform.position.x, GameObject.Find("platform1").transform.position.y)){
+                transform.position = GameObject.Find("platform1").transform.position;
+            }
+            else if (this.check(GameObject.Find("platform2").transform.position.x, GameObject.Find("platform2").transform.position.y)){
+                transform.position = GameObject.Find("platform2").transform.position;
+            }
+            else if (this.check(GameObject.Find("platform3").transform.position.x, GameObject.Find("platform3").transform.position.y)){
+                transform.position = GameObject.Find("platform3").transform.position;
+            }
+            else if (this.check(GameObject.Find("platform4").transform.position.x, GameObject.Find("platform4").transform.position.y)){
+                transform.position = GameObject.Find("platform4").transform.position;
+            }
+            else if (this.check(GameObject.Find("platform5").transform.position.x, GameObject.Find("platform5").transform.position.y)){
+                transform.position = GameObject.Find("platform5").transform.position;
+            }
+            else if (this.check(GameObject.Find("platform6").transform.position.x, GameObject.Find("platform6").transform.position.y)){
+                transform.position = GameObject.Find("platform6").transform.position;
+            }
+            else {
+                transform.position = (Vector2) Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            }
+
+            this.pos_x = transform.position.x;
+            this.pos_y = transform.position.y; 
+        }
     }
     private void OnMouseOver(){
         if (isDraggable && Input.GetMouseButtonDown(0)) {
