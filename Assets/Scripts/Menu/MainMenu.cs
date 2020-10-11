cd62df8d7abf725b2477a37eb0b6cc068dd5d14b
satorhho <lbriones@gbox.adnu.edu.ph>
2020-10-11 20:09:25 +0800

    Added: Main Menu + Fix: Build Heirarchy

---
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+    public void play_game() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void quit_game() {
+        Debug.Log("Nag quit na po hehehe @_@");
+        Application.Quit();
+    }
+
+}
