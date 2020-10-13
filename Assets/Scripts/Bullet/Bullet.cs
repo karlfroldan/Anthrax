62e0620e065e61640b5bc6277dce3613160f70ad
Karl Roldan <36784362+karlfroldan@users.noreply.github.com>
2020-10-13 18:14:59 +0800

    gay

---
+++ Assets/Scripts/Bullet/Bullet.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Rigidbody2D m_RigidBody;
+    void Start()
+    {
+
+        m_RigidBody = gameObject.GetComponent<Rigidbody2D>();
+        Debug.Log("is m_RigidBody simulated" + m_RigidBody.simulated);
+    }
+
+    // Update is called once per frame
+    public void Init(Vector3 direction)
+    {
+        Debug.Log("We are now shooting");
+        m_RigidBody.AddForce(direction);
+    }
+}
