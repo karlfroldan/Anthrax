4d6e488391d699bab70731d726d6940aebf19af3
satorhho <lbriones@gbox.adnu.edu.ph>
2020-10-20 15:05:26 +0800

    Drag and drop changes (2)

--- Assets/Scripts/Tower/DragDropDoctor.cs
+++ Assets/Scripts/Tower/DragDropDoctor.cs
@@ -12,14 +12,18 @@ public class DragDropDoctor : MonoBehaviour{
     private const double original_pos_x = 13.26;      // use this to snapback to original x position
     private const double original_pos_y = -2.07;       // use this to snapback to original y position
 
-    private bool isSnappedToPlatform = false;
+    // KARL THIS IS YOUR CODE
+    //private bool isSnappedToPlatform = false;
+    //private bool isCurrentPlatformAssigned = false;
+    // END OF YOUR CODE
+
     private bool isSticked = false;
     private bool isReleased;
     // list of platforms to be used
     public List<GameObject> platforms;
 
     private bool check(float x, float y) {
-        if ( (this.pos_x <= x+1 && this.pos_x >= x-1) && (this.pos_y <= y+1 && this.pos_y >= y-1) ){
+        if ( (this.pos_x <= x+1.5 && this.pos_x >= x-1.5) && (this.pos_y <= y+1.5 && this.pos_y >= y-1.5) ){
             return true;
         }
 
@@ -27,13 +31,12 @@ public class DragDropDoctor : MonoBehaviour{
     }
     void Update(){
         GameObject currentPlatform = platforms[0];
-        bool isCurrentPlatformAssigned = false;
 
-        if (isDragged){
-            // check if there's a platform that satisfies the condition that it has the same position vector
-            // as our tower.
+        if (this.isDragged){
             transform.position = (Vector2) Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
+        }    
+        else {
             foreach(GameObject platform in platforms)
             {
                 if(check(platform.transform.position.x, platform.transform.position.y))
@@ -41,37 +44,40 @@ public class DragDropDoctor : MonoBehaviour{
                     transform.position = platform.transform.position;
                     this.isSticked = true;
                     this.isDraggable = false;
+
+                    // KARL THIS IS YOUR CODE
+
                     currentPlatform = platform;
-                    isCurrentPlatformAssigned = true;
+                    //isCurrentPlatformAssigned = true;
+
+                    // END OF YOUR CODE
                 }
             }
-
-            if(!isCurrentPlatformAssigned)
-            {
-                if(!isSnappedToPlatform)
-                {
-                    isSnappedToPlatform = true;
-                    // create a GameObject that does the shooting
-                    GameObject towerShooterObject = new GameObject("Tower Shooter Object");
-                    towerShooterObject.AddComponent<TowerShooting>();
-                    TowerShooting towerShooter = towerShooterObject.GetComponent<TowerShooting>();
-                    Debug.Log("Game Object name: " + gameObject.name);
-                    towerShooter.parentName = gameObject.name;
-                    towerShooter.SetColliderRadius(7.4f);
-                    towerShooter.SetPosition(gameObject.transform.position);
-                }
-                
-            }
-
         }
-        else {
-            if(!this.isSticked) {
-                transform.position = new Vector2((float)original_pos_x, (float)original_pos_y);
-            }
-                
+
+        if(!this.isSticked && !this.isDragged) {
+            transform.position = new Vector2((float)original_pos_x, (float)original_pos_y);    
         }
+
         this.pos_x = transform.position.x;
-        this.pos_y = transform.position.y; 
+        this.pos_y = transform.position.y;
+
+        // KARL THIS IS YOUR CODE
+
+        if(this.isSticked)
+        {
+            // create a GameObject that does the shooting
+            GameObject towerShooterObject = new GameObject("Tower Shooter Object");
+            towerShooterObject.AddComponent<TowerShooting>();
+            TowerShooting towerShooter = towerShooterObject.GetComponent<TowerShooting>();
+            Debug.Log("Game Object name: " + gameObject.name);
+            towerShooter.parentName = gameObject.name;
+            towerShooter.SetColliderRadius(7.4f);
+            towerShooter.SetPosition(gameObject.transform.position);
+        }
+        
+        // END OF YOUR CODE
+
     }
     private void OnMouseOver(){
         if (isDraggable && Input.GetMouseButtonDown(0)) {
